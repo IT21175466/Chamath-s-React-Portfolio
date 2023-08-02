62ece138ad77d3b6142b52ab7df292d8c1a36186
Chamath Harshana <it21175466@my.sliit.lk>
2023-08-02 12:41:13 +0530

    CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import { useEffect } from "react";
+import Aos from "aos";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <h1 className="text-indigo-600 text-2xl font-[500]">Hello World</h1>
 
   );
 }
