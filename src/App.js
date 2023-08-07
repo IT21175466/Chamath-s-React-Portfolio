7e54a81addb28d561f8dd2bb037b6e67e048b5f6
Chamath Harshana <it21175466@my.sliit.lk>
2023-08-07 10:36:43 +0530

    Contact Page Done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/UI/Hero";
 import Services from "./components/UI/Services";
 import Portfolio from "./components/UI/Portfolio";
+import Contact from "./components/UI/Contact";
 
 function App() {
   useEffect(() => {
@@ -18,6 +19,7 @@ function App() {
             <Hero />
             <Services />
             <Portfolio />
+            <Contact />
         </main>
         <Footer/>
       </>
